ad877bca9009cb8cc00b6131f316f37a812afdb6
Tobias Badertscher <tobias.badertscher@baerospace.ch>
2021-09-06 20:53:33 +0200

    Work in progress.

--- iqPuzzle.py
+++ iqPuzzle.py
@@ -41,12 +41,13 @@ figure[11]={'color':BLUE, 'tile':[[1,1,1], [0,0,1], [0,0,1]],  'defPlc':(0,0,0,0
 placedFigures=[(None, None, None )]*len(figure)
 configurations=[{}]*5
 #confiurations[0]={'figOR':[[0,1,1], [1,0,0], [11,0,0], [7,1,1]],'pos':((0,0), (3, 3),(5,0),(5,3))}
-configurations[0]={'figOR':[[1,1,0], [11,0 ,0], [7,0,0]],'pos':((0,0), (0, 2), (2,1),(5,3))}
+configurations[0]={'figOR':[[1,1,0], [11,0 ,0], [7,0,0]],'pos':((0,0), (3,2), (0,3),(5,3))}
 print(configurations)
 
 gridSize=25
 size =  gridSize-6
-playField = (11, 5)
+playField = ((0,11), (0,5))
+placementRange=((13,17),(1,5))
 boardSize =(19,19)
 
 pygame.init()
@@ -65,8 +66,8 @@ def draw(surface ,pf):
     pygame.display.update()
 
 def initPlayField(board):
-    for x in range(playField[0]) :
-        for y in range(playField[1]):
+    for x in range(playField[0][1]) :
+        for y in range(playField[1][1]):
             xp=gridSize*x
             yp=gridSize*y
             board[x][y] = WHITE
@@ -80,13 +81,13 @@ def addFigure(board,fig, xpos, ypos ,mirror, orientation, alwaysPlace):
     # -1 : Rotate counter clock wise
     #  0 : Place as it is
     #  1 : Rotate clock wise
-    # always plays
+    # always place
     xposMax=xpos
     yposMax=ypos
     sizex=len(fig['tile'][0])
     sizey=len(fig['tile'])
-    bsizex=playField[0]
-    bsizey=playField[1]
+    bsizex=playField[0][1]
+    bsizey=playField[1][1]
     if orientation==0:
         xposMax+=sizex
         yposMax+=sizey
@@ -100,9 +101,10 @@ def addFigure(board,fig, xpos, ypos ,mirror, orientation, alwaysPlace):
     if not (ypos<bsizey and yposMax<=bsizey) :
         print("y: %d, %d<=%d"%(ypos, yposMax, bsizey))
         print("Drop figure due to ypos")
-    if not (xpos<bsizex and xposMax<=bsizex and ypos<= bsizey and yposMax<=bsizey) and alwaysPlace:
-        print("Drop figure")
-        return -1,-1
+    if not alwaysPlace:
+        if not (xpos<bsizex and xposMax<=bsizex and ypos<= bsizey and yposMax<=bsizey):
+            print("Drop figure due to not in valid range")
+            return -1,-1
     print('figure', fig, sizex, sizey)
     xseq=list(range(sizex))
     print(xseq)
@@ -149,20 +151,29 @@ def getPlaygroundPos(x,y):
     print(x,y)
     return x,y
 
+def isInisPlacementRange(x,y,posRange):
+    print(x,y, posRange,)
+    isPlacement= x>=posRange[0][0] and x<posRange[0][1]
+    print(isPlacement)
+    isPlacement&= y>=posRange[1][0] and y<posRange[1][1]
+    print(isPlacement)
+    return isPlacement
+    
 #initPlayField(board)
 #print(board)
 
 initPlayField(board)
 for i in range(len(figure)):
     print(figure[i])
-    addFigure(board, figure[i],figure[i]['pos'][1], figure[i]['pos'][0],0,0, False)
+    addFigure(board, figure[i],figure[i]['pos'][1], figure[i]['pos'][0],0,0, True)
     addKeystoneName(figure[i])
+
 for i in range(len(configurations[0]['figOR'])):
     print( "figOR", configurations[0]['figOR'] )
     figIdx= configurations[0]['figOR'][i][0]
     x=configurations[0]['pos'][i][0]
     y=configurations[0]['pos'][i][1]
-    addFigure(board, figure[figIdx], x, y, configurations[0]['figOR'][i][1],configurations[0]['figOR'][i][2], False)
+    addFigure(board, figure[figIdx], x, y, configurations[0]['figOR'][i][1],configurations[0]['figOR'][i][2], True)
 
 selectedKey=-1
 while True:
@@ -178,9 +189,21 @@ while True:
         if event.type == MOUSEBUTTONUP:
             print(event.pos[0], event.pos[1])
             x,y=getPlaygroundPos(event.pos[0], event.pos[1])
-            if (x>=0 and y>=0) and selectedKey>=0:
-                print("Place at Location (%d, %d)" % (x, y))
-                addFigure(board, figure[selectedKey], x, y, configurations[0]['figOR'][j][1],configurations[0]['figOR'][j][2], True)
+            isPlayGroundRange=isInisPlacementRange(x,y,playField)
+            isPlaceRange=isInisPlacementRange(x,y,placementRange)
+            if selectedKey>=0:
+                print("Place at Location (%d, %d), inRange %d, inPlace %d" % (x, y,isPlayGroundRange,isPlaceRange))
+                if isPlayGroundRange:
+                    addFigure(board, figure[selectedKey], x, y, 0,0, True)
+                if isPlaceRange:
+                    print("Place in Placerange")
+                    addFigure(board, figure[selectedKey], placementRange[0][0], placementRange[1][0], 0,0, True)
+        if event.type == KEYDOWN and (event.key == "R" or event.key=="r"):
+            x,y = pygame.mouse.get_pos()
+            x/=gridSize
+            y/=gridSize
+            if (x > placementRange[0][0] and x<placementRange[1][0]) and ( y > placementRange[1][1] and y<placementRange[1][1]):
+                print("In range")
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
